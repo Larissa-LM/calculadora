350e144679dc5989a805da0849769db6ba3a4338
Larissa-LM <larissa.maria@creditas.com>
2022-03-11 19:45:04 -0300

    Função de soma criada

--- calculadora.js
+++ calculadora.js
@@ -1,7 +1,36 @@
+
+let numero = 0;
+let resultado = null; 
+let operacao =" " ;
+
 function clickTeclado(num){
-     document.getElementById("display").value += num;
-    
+    document.getElementById("display").value += num;
+    numero = Number(document.getElementById("display").value);
+
 }
+
 function limpar() {
     document.getElementById("display").value =" ";
+    numero = 0;
+    resultado = 0;
+}
+
+function soma(){
+    operacao="+";
+    if(resultado == null){
+      resultado = numero;
+    }else {
+      resultado += numero; 
+
+    }
+    console.log("recebendo resultado" + resultado);
+    document.getElementById("display").value = " ";
+}
+
+function mostraResultado(){
+    console.log(operacao);
+    if(operacao == "+"){
+        soma();
+    }
+    document.getElementById("display").value = resultado.toString();
 }
