aabd0442a44e871bd35d42c703763117e2a5c0a9
Larissa-LM <larissa.maria@creditas.com>
2022-03-11 21:32:32 -0300

    Refatorando função soma e criando função subtração

--- calculadora.js
+++ calculadora.js
@@ -4,33 +4,74 @@ let resultado = null;
 let operacao =" " ;
 
 function clickTeclado(num){
-    document.getElementById("display").value += num;
-    numero = Number(document.getElementById("display").value);
+  document.getElementById("display").value += num;
+  numero = Number(document.getElementById("display").value);
 
 }
 
 function limpar() {
-    document.getElementById("display").value =" ";
-    numero = 0;
-    resultado = 0;
+  document.getElementById("display").value =" ";
+  numero = 0;
+  resultado = null;
 }
-
+function verificarOperacao(){
+  switch(operacao){
+    case "+":
+      resultado += numero;
+      break;
+    case "-":
+      resultado -= numero;
+      break;
+    case "*":
+      resultado *= numero;
+      break;
+    case "/":
+      resultado /= numero;
+      break;
+  }
+}
 function soma(){
-    operacao="+";
-    if(resultado == null){
-      resultado = numero;
-    }else {
-      resultado += numero; 
 
-    }
-    console.log("recebendo resultado" + resultado);
-    document.getElementById("display").value = " ";
+  if(resultado == null){
+    resultado = numero;
+
+  }else {
+    verificarOperacao();
+  }
+
+  operacao = "+"; 
+  //console.log("recebendo resultado" + resultado);
+  document.getElementById("display").value = " ";
 }
 
+function subtracao() {
+
+  if(resultado == null){
+    resultado = numero;
+  }else {
+    verificarOperacao(); 
+  }
+
+  operacao="-";
+  console.log("recebendo resultado" + resultado);
+  document.getElementById("display").value = " ";
+}
+
+function multiplicacao (){}
+
+function divisao(){}
 function mostraResultado(){
-    console.log(operacao);
-    if(operacao == "+"){
-        soma();
-    }
-    document.getElementById("display").value = resultado.toString();
+
+  console.log(operacao);
+
+  switch(operacao){
+    case "+":
+      soma();
+      break;
+    case "-":
+      subtracao();
+      break;
+  } 
+
+  document.getElementById("display").value = resultado.toString();
 }
